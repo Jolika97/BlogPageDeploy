f11796c1b5f8fdc013d274c48cf8c1fb0987c976
jolikasulejmani <83695798+jolikasulejmani@users.noreply.github.com>
2021-09-16 12:04:51 +0200

    Update BlogPageBeApplicationTests.java

--- src/test/java/com/crystal/blogpagebe/BlogPageBeApplicationTests.java
+++ src/test/java/com/crystal/blogpagebe/BlogPageBeApplicationTests.java
@@ -1,12 +1,7 @@
 package com.crystal.blogpagebe;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class BlogPageBeApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
